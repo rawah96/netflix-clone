7e8e832de02954702f9aaa0177d3aae2ad65144b
rawah <rawah@pdx.edu>
2020-09-13 01:49:55 -0700

    done with the banner

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 import Row from './components/Row/Row'
 import requests from './requests';
+import Banner from './components/Banner/Banner';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Banner />
       <Row 
         title="NETFLEX ORIGINALS"
         fetchUrl={requests.fetchNetflexOriginals}
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import './Banner.css'
+import axios from '../../axios.js'
+import requests from '../../requests.js'
+
+function Banner() {
+    // responsible for whatever random movie is being selected
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const req = await axios.get(requests.fetchNetflexOriginals);
+            setMovie(
+            req.data.results[Math.floor(Math.random() * req.data.results.length -1)]);
+            return req;
+        }
+
+        fetchData();
+    }, []);
+
+    console.table(movie);
+    const truncate = (str, n) => {
+        if(str && str.length > n) {
+            return (str.substr(0, n-1) + "...")
+        } else {
+            return str;
+        }
+    }
+    return (
+        <header className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+                backgroundPosition: "center center"
+            }}
+        >
+            <div className="content">
+                <h1>{movie.title || movie.original_name || movie.name}</h1>
+                <div className="banner-btns">
+                    <button className="banner-btn">Play</button>
+                    <button className="banner-btn">My List</button>
+                </div>
+                <h1 className="description">{truncate(movie.overview, 150)}</h1>
+            </div>
+
+            <div className="fade-bottom" />
+        </header>
+    )
+}
+
+export default Banner
