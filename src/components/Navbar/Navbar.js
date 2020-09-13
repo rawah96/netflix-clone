4d5f98c92ebd61c91507a20f02994fc279d5310e
rawah <rawah@pdx.edu>
2020-09-13 02:11:17 -0700

    done with the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Navbar.css'
+import netflix from './netflix.png'
+import avatar from './avatar.jpg'
 
 function Navbar() {
+    const [show, handleShow] = useState(false);
+    // adding scroll listener
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener('scroll');
+        }
+    },[])
     return (
-        <div className="navbar">
-            
+        // always gonna have navbar, but if show is true, use the other classname
+        <div className={`navbar ${show && "nav-black"}`}>
+            <img className="logo" src={netflix} alt="" />
+
+            <img className="avatar" src={avatar} alt="" />
         </div>
     )
 }
