35a86e58450c4797d75d8de4994747efc37562d0
rawah <rawah@pdx.edu>
2020-09-13 02:47:14 -0700

    Updates

---
+++ precache-manifest.d867781d71e8f239da07e55f2d0c8865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf07a566d93a9c020b03130ae637a5c",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "4322564c5a9bf8e4c9f8",
+    "url": "/netflix-clone/static/css/main.5db515d1.chunk.css"
+  },
+  {
+    "revision": "4ea1aad429f6942508a8",
+    "url": "/netflix-clone/static/js/2.2f113e85.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.2f113e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4322564c5a9bf8e4c9f8",
+    "url": "/netflix-clone/static/js/main.cd404dc4.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  },
+  {
+    "revision": "e9706859c2929c437bb8bd7ee49a2dcf",
+    "url": "/netflix-clone/static/media/netflix.e9706859.png"
+  }
+]);
